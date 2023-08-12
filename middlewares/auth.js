b5e3d659ec0e8518842d67ba118915277cc012da
lesha <oookosmos.m@yandex.ru>
2023-08-12 21:43:51 +0300

    подгон под фронт

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,21 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../errors/Unauthorized');
+require('dotenv').config();
 
 const { NODE_ENV, JWT_SECRET } = process.env;
+const JwtSecretToken = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
 
-module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new Unauthorized('Пользователь не авторизован.');
+module.exports.checkAuthorizedUser = (req, res, next) => {
+  const cookieJWT = req.cookies.jwt;
+  if (!cookieJWT) {
+    return next(new Unauthorized('Необходима авторизация'));
   }
-
-  const token = authorization.replace('Bearer ', '');
-
   let payload;
-
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(cookieJWT, JwtSecretToken);
   } catch (err) {
-    throw new Unauthorized('Пользователь не авторизован.');
+    return next(new Unauthorized('Необходима авторизация'));
   }
-
   req.user = payload;
-  next();
+  return next();
 };
--- models/movie.js
+++ models/movie.js
@@ -1,74 +1,61 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const checkUrl = (url) => validator.isURL(url);
 
-const RegExpURL = /^(https?:\/\/(www\.)?)([-a-zA-Z0-9\W]){1,}/;
-const RegExpEN = /[A-z]/;
-const RegExpRU = /[А-я]/;
+const requiredString = (string, min = 3, max = false, required = true) => ({
+  type: String,
+  ...(min && { minlength: [min, `${string} не может быть короче ${min} символов`] }),
+  ...(max && { maxlength: [max, `${string} не может быть длиннее ${max} символов`] }),
+  ...(required && { required: [ true, `${string} не может быть пустым`] }),
+});
+
+const urlString = (string) => ({
+  ...requiredString(string),
+  validate: {
+    validator: checkUrl,
+    message: `Ссылка для поля ${string.toLowerCase()} некорректна`,
+  }
+});
 
 const movieSchema = new mongoose.Schema({
   country: {
-    type: String,
-    required: true,
+    ...requiredString('Страна', 3, 80),
   },
   director: {
-    type: String,
-    required: true,
+    ...requiredString('Режиссёр', 2, 80),
   },
   duration: {
     type: Number,
-    required: true,
+    min: [1, 'Продолжительность должна быть не меньше 1'],
+    required: [true, 'Продолжительность не может быть пустой'],
   },
   year: {
-    type: String,
-    required: true,
+    min: [1888, 'Год должен быть начиная с 1895'],
+    max: [new Date().getFullYear(), 'Год не может быть больше текущего'],
+    ...requiredString('Год', 4, 4),
   },
   description: {
-    type: String,
-    required: true,
+    ...requiredString('Описание', 10),
   },
-  image: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (url) => RegExpURL.test(url),
-    },
-  },
-  trailerLink: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (url) => RegExpURL.test(url),
-    },
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (url) => RegExpURL.test(url),
-    },
-  },
+  image: urlString('Изображение'),
+  trailerLink: urlString('Ссылка на трейлер'),
+  thumbnail: urlString('Миниатюрное изображение'),
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    required: true,
+    required: [true, 'Владелец не может быть пустым'],
   },
   movieId: {
     type: Number,
-    required: true,
+    min: [1, 'ID фильма должен быть не меньше 1'],
+    required: [true, 'ID фильма не может быть пустым'],
   },
   nameRU: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (name) => RegExpRU.test(name),
+      ...requiredString('Название на русском', 2, 70),
     },
-  },
   nameEN: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (name) => RegExpEN.test(name),
+      ...requiredString('Название на английском', 2, 70),
     },
-  },
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('movie', movieSchema);
