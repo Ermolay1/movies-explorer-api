031c09c4b16559665a572b42bc61bcccdc7594dd
lesha <oookosmos.m@yandex.ru>
2023-08-15 03:12:33 +0300

    d

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
 const limiter = require('./utils/limiter');
 const routes = require('./routes/index');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
-
+// eslint-disable-next-line spaced-comment
+//const cors = require('./middlewares/cors');
+// eslint-disable-next-line semi, import/newline-after-import
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const app = express();
 
@@ -17,7 +20,7 @@ app.use(express.json());
 mongoose.connect(DB_URL);
 
 app.use(helmet());
-app.use(cors);
+app.use(cors());
 
 app.use(requestLogger);
 
